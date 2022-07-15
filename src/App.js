914cc2bca0c264738df6cd7d272c698141ebeaf7
Sydney McGee <mcgee.sydney@gmail.com>
2022-07-14 22:16:19 -0500

    Created DisplayEntries component and instantiated component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import React, { useState } from 'react';
+import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
 
 
 function App() {
 
-  const [entries, setEntries] = useState([{weight: 175, date: "11-23-2021"}])
+  const [entries, setEntries] = useState([{weight: 175, date: "11-23-2021"}, {weight: 256, date: "10-23-2021"}])
   return (
     <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Entry Number</th>
-            <th>Weight</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>175</td>
-            <td>"11-23-2021"</td>
-          </tr>
-        </tbody>
-      </table>
+      <DisplayEntries parentEntries={entries} />
     </div>
   );
 }
